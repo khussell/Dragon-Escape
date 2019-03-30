af1b0316b197eebc81412cd805179546c2267820
Lilu <KHussell@gmail.com>
2019-03-30 15:39:26 -0400

    more style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -61,6 +61,38 @@ var game={
        //background changes w/ new dragon
        backgroundImage += 1
        directText.text("Yay! The dragon let you pass, but there's another!")
+       if(backgroundImage === 1){
+        $(".everything").css("background-image", "url(../images/cave3.jpg)")
+        $(".everything").css("top", "-100px")
+    $(".everything").css("opacity", ".65")
+       $("#dragon3").css("display", "none")
+       $("#dragon1").css("display", "block")
+    }  else if(backgroundImage === 2){
+        $(".everything").css("background-image", "url(../images/cave2.jpg)")
+        $(".everything").css("opacity", ".65")
+        $("#dragon1").css("display", "none")
+        $("#dragon2").css("display", "block")
+    } else  if(backgroundImage === 3){
+        $(".everything").css("background-image", "url(../images/cave4.jpg)")
+        $(".everything").css("opacity", ".65")
+        $("#dragon2").css("display", "none")
+        $("#dragon4").css("display", "block")
+    }  else if(backgroundImage === 4){
+        $(".everything").css("background-image", "url(../images/cave5.jpg)")
+        $(".everything").css("opacity", ".65")
+        $("#dragon4").css("display", "none")
+        $("#dragon5").css("display", "block")
+    }   else if(backgroundImage === 5){
+        $(".everything").css("background-image", "url(../images/cave6.jpg)")
+        $(".everything").css("opacity", ".65")
+        $(".everything").css("top", "-100px")
+        $("#dragon5").css("display", "none")
+        $("#dragon6").css("display", "block")
+    }else{
+        $(".everything").css("background-image", "url(../images/cave7.jpg)")
+        $(".everything").css("opacity", ".65")
+        $(".everything").css("top", "-100px")
+    }
        this.reset()
     },
 
@@ -68,7 +100,7 @@ var game={
         losses+= 1
         lossesText.text("Losses: " + losses)
         if(losses)
-        directText.text("Oh noooo! Don't worry the dragon is merciful, try again")
+        directText.text("Oh noooo! Don't worry the dragon is merciful, try again.")
 
         this.reset()
     },
@@ -87,6 +119,14 @@ var game={
         crystalNum4 = (Math.floor(Math.random()*12)+1)
         crystalNumArray.push(crystalNum4)
         crystalMin= Math.min(...crystalNumArray)
+    },
+
+    resetDirect: function(){
+        if(directText.text()=== "Oh noooo! Don't worry the dragon is merciful, try again."){
+            directText.text("Click a crystal!")
+        } else if(directText.text() === "Yay! The dragon let you pass, but there's another!"){
+            directText.text("Click a crystal!")
+        }
     }
 
 
@@ -108,22 +148,26 @@ $(document).ready(function(){
   crystalMin= Math.min(...crystalNumArray)
 
 $("#crys1").on("click", function(){
+    game.resetDirect()
     score += crystalNum1
     scoreText.text(score)
     game.winOrLose()
 })
 $("#crys2").on("click", function(){
+    game.resetDirect()
     score += crystalNum2
     scoreText.text(score) 
     game.winOrLose()
  
 })
 $("#crys3").on("click", function(){
+    game.resetDirect()
     score += crystalNum3
     scoreText.text(score) 
     game.winOrLose()
 })
 $("#crys4").on("click", function(){
+    game.resetDirect()
     score += crystalNum4
     scoreText.text(score) 
     game.winOrLose()
