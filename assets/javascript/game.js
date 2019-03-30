51f855a5991eadc312f2546aa3fd0e371740d2c7
Lilu <KHussell@gmail.com>
2019-03-30 16:02:16 -0400

    need to tweak math and losing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -62,34 +62,34 @@ var game={
        backgroundImage += 1
        directText.text("Yay! The dragon let you pass, but there's another!")
        if(backgroundImage === 1){
-        $(".everything").css("background-image", "url(../images/cave3.jpg)")
+        $(".everything").css("background-image", "url(https://www.doc.govt.nz/thumbs/hero/contentassets/91ab267577544e9cb6c4a15638cbdfc8/waipu-caves-hero.jpg)")
         $(".everything").css("top", "-100px")
     $(".everything").css("opacity", ".65")
        $("#dragon3").css("display", "none")
        $("#dragon1").css("display", "block")
     }  else if(backgroundImage === 2){
-        $(".everything").css("background-image", "url(../images/cave2.jpg)")
+        $(".everything").css("background-image", "url(https://static.turbosquid.com/Preview/2016/09/17__16_07_56/CrystalCavern1.jpg5BD427A7-8D9B-4F8E-92E7-D81B7D7407DFOriginal.jpg)")
         $(".everything").css("opacity", ".65")
         $("#dragon1").css("display", "none")
         $("#dragon2").css("display", "block")
     } else  if(backgroundImage === 3){
-        $(".everything").css("background-image", "url(../images/cave4.jpg)")
+        $(".everything").css("background-image", "url(https://irp-cdn.multiscreensite.com/31cb13a1/dms3rep/multi/tablet/FormationsRoom-3400x2550.jpg)")
         $(".everything").css("opacity", ".65")
         $("#dragon2").css("display", "none")
         $("#dragon4").css("display", "block")
     }  else if(backgroundImage === 4){
-        $(".everything").css("background-image", "url(../images/cave5.jpg)")
+        $(".everything").css("background-image", "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlFbZ9SLSbmy5DNxZcDrhq_otjSUgUlY85m1t9DzWYY6hq4L0j)")
         $(".everything").css("opacity", ".65")
         $("#dragon4").css("display", "none")
         $("#dragon5").css("display", "block")
     }   else if(backgroundImage === 5){
-        $(".everything").css("background-image", "url(../images/cave6.jpg)")
+        $(".everything").css("background-image", "url(https://cdn1.img.sputniknews.com/images/105728/33/1057283307.jpg)")
         $(".everything").css("opacity", ".65")
         $(".everything").css("top", "-100px")
         $("#dragon5").css("display", "none")
         $("#dragon6").css("display", "block")
     }else{
-        $(".everything").css("background-image", "url(../images/cave7.jpg)")
+        $(".everything").css("background-image", "url(https://www.chromethemer.com/wallpapers/chromebook-wallpapers/download/italian-beach-3840x2160.jpg)")
         $(".everything").css("opacity", ".65")
         $(".everything").css("top", "-100px")
     }
@@ -99,10 +99,15 @@ var game={
     lose: function(){
         losses+= 1
         lossesText.text("Losses: " + losses)
-        if(losses)
+        if(losses === 5){
+            directText.text("The dragons were merciful for a time, but they want to brunch now!")
+            $(".everything").css("background-image", "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFZ8GMZfdwgzJtAJdyFc7EeVwypRCEoofnf4qC1u9b3mdy6_KNOw)")
+            $(".everything").css("opacity", "1")
+        }else{
         directText.text("Oh noooo! Don't worry the dragon is merciful, try again.")
-
+       
         this.reset()
+    }
     },
 
     reset: function(){
